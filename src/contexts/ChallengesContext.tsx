9aa8119b996aff44afe5714e2fe366cb372a3411
Gabriel de Barros Pontes <gabrielpontes98@gmail.com>
2021-02-27 15:36:40 -0300

    Adicionando arquivos - Primeiro Commit

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,102 @@
+import { createContext, useState, ReactNode, useEffect } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge{
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
+
+interface ChallengesContextData{
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
+    levelUp: () => void;
+    startNewChallenge: () => void;
+    activeChallenge: Challenge;
+    resetChallenge: () => void;
+    experienceToNextLevel: number;
+    completeChallenge: () => void;
+}
+
+interface ChallengesProviderProps{
+    children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({children} : ChallengesProviderProps){
+
+    const [level, setLevel] = useState(1);
+    const [currentExperience, setCurrentExperience] = useState(0);
+    const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+    const [activeChallenge, setActiveChallenge] = useState(null)
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
+
+    function levelUp(){
+        setLevel(level + 1);
+    }
+
+    function startNewChallenge(){
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+        const Challenge = challenges[randomChallengeIndex]
+        
+        setActiveChallenge(Challenge) 
+
+        new Audio('/notification.mp3').play();
+        
+        if (Notification.permission === 'granted'){
+            new Notification('Novo desafio', {
+                body: `Valendo ${Challenge.amount}xp!`
+            })
+        }
+    }
+
+    function resetChallenge(){
+        setActiveChallenge(null);
+    }
+
+    function completeChallenge(){
+        if(!activeChallenge){
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        // Let it change - Variável que pode mudar
+        let finalExperience = currentExperience + amount;
+
+        if(finalExperience >= experienceToNextLevel){
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+    
+    return(
+        <ChallengesContext.Provider
+            value={{
+                level,
+                currentExperience,
+                challengesCompleted,
+                levelUp,
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge,
+                experienceToNextLevel,
+                completeChallenge
+            }}
+        >
+            {children}
+        </ChallengesContext.Provider>
+    );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css'
+
+import { ChallengesProvider} from '../contexts/ChallengesContext'
+import { CountdownProvider } from '../contexts/CountdownContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChallengesProvider>
+      <CountdownProvider>
+        <Component {...pageProps} /> 
+      </CountdownProvider>       
+    </ChallengesProvider>
+  )
+}
+
+export default MyApp
